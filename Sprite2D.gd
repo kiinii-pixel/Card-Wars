925a77c9e8d960abb5b3058bb31526c7792348f4
kiinii <kiiniimail@gmail.com>
2023-11-05 19:03:37 +0100

    Godot Doc Testing
    Godot icon can be moved forward (up), left and right. There is a button that can disable processing.

---
+++ Sprite2D.gd
@@ -0,0 +1,23 @@
+extends Sprite2D
+
+var speed = 400
+var angular_speed = PI
+
+func _process(delta):
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+
+	rotation += angular_speed * direction * delta
+	
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
+	
+	position += velocity * delta
+
+
+func _on_button_pressed():
+	set_process(not is_processing())
