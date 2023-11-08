dfbff517fb76cd9dd91cc88f8ad9e0927ae7e701
kiinii <kiiniimail@gmail.com>
2023-11-08 17:28:00 +0100

    Edited Card Class and Imported Resources
    Imported various resources and worked on card Class to load given text into Attack, Defence and Cost text labels.

--- Card.gd
+++ Card.gd
@@ -1,18 +1,19 @@
 extends Node2D
 
-@export var cost = 1
+var cost = 1
 var attack = 1
 var defense = 2
-@export var card_type = ['Creature', 'Building', 'Spell']
+var card_type = ['Creature', 'Building', 'Spell']
 var landscape
 var floop
 var zone
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Sprite2D/Attack_Label.text = String.num_int64(attack)
-	$Sprite2D/Defense_Label.text = String.num_int64(defense)
-	$Sprite2D/Cost_Label.text = String.num_int64(cost)
+	#get_node("CardImage/Text/CCAttack/AttackLabel").text = String.num_int64(attack) //same as:
+	$CardImage/Text/CCAttack/AttackLabel.text = String.num_int64(attack)
+	$CardImage/Text/CCDefense/DefenseLabel.text = String.num_int64(defense)
+	$CardImage/Text/CCCost/CostLabel.text = String.num_int64(cost)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
