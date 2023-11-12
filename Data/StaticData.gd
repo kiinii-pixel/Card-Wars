3b06d84277f3a91d99c3bddcc62883535fa25d1f
kiinii <kiiniimail@gmail.com>
2023-11-12 22:10:33 +0100

    Card Data is working!!
    Every Card's data is now stored in a dictionary, with each card being its own dictionary.

--- Data/StaticData.gd
+++ Data/StaticData.gd
@@ -1,46 +1,20 @@
 extends Node
 
 
-#var dict = {}
+var CardData = {}
+var data_file_path = "res://Data/all_cards.json"
 
 func _ready():
-	pass
-	
-	#ASK GODOT:
-#	var file = new()
-#	file.open("res://Data/StaticData.json")
-#	var text = file.get_as_text()
-#	dict.parse_json(text)
-#	file.close()
-#	# print something from the dictionnary for testing.
-#	print(dict["name"])
+	CardData = load_json_file(data_file_path)
 
-	
-	#CHAT GPT:
-	var json_text = preload("res://Data/allcards.json").get_parsed_text()
-	var json_text2 = preload("res://Data/allcards.json")
-	json_text2.get_parsed_text()
-	var result = json_text.json_escape()
-	print(json_text)
-#	if result == OK:
-#		var json_dict = json_data.get_data()
-#	else:
-#		print("Failed to parse JSON")
-
-#	DOCUMENTATION
-#	var json_string = preload("res://Data/allcards.json").get_text()
-#	var json = JSON.new()
-#	var error = json.parse(json_string)
-#	if error == OK:
-#		var data_received = json.data
-#		if typeof(data_received) == TYPE_ARRAY:
-#			print(data_received) # Prints array
-#		else:
-#			print("Unexpected data")
-#	else:
-#		print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+func load_json_file(filePath : String):
+	if FileAccess.file_exists(filePath):
+		var dataFile = FileAccess.open(filePath, FileAccess.READ)
+		var parsedResult = JSON.parse_string(dataFile.get_as_text())
+		
+		if parsedResult is Dictionary:
+			return parsedResult
+		else:
+			print("error reading file")
+	else:
+		print("doesnt exist")
--- Scripts/Card.gd
+++ Scripts/Card.gd
@@ -16,8 +16,6 @@ func _ready():
 	$CardImage/Text/CCCost/CostLabel.text = String.num_int64(cost)
 	
 	
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(_delta): 
 	pass
