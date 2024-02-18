354257f9391870ec0a3d5552f8780bb59c7ebc8e
kiinii <kiiniimail@gmail.com>
2024-02-19 00:18:45 +0100

    Create Deck Resource
    Started Resource Implementation.
    Trying out custom resources, made card array and character variables so far.

---
+++ scripts/deck_resource.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name Deck
+
+@export_enum("Finn", "Jake", "BMO") var character : int
+
+var deck = [
+7, 501, 505, 460, 499, 81, 114, 124, 215, 340, 344, 437, 480,
+496, 498, 452, 33, 65, 384, 497, 190, 67
+]
