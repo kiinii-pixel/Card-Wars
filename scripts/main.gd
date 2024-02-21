90f3eac4123ba77260f7f0e966ba2ce9b7065930
kiinii <kiiniimail@gmail.com>
2024-02-21 15:59:38 +0100

    Removed Resource Creator Function
    Removed the Resource Creator, cause they're already created. This is a big step towards Deck functionality. I should be able to Make a deck resource and just drag and drop indivdual Card Resources into it to create a Deck.
    side note: i'm a genius

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 func _ready():
-	create_resources()
+	#create_resources()
+	pass
 
 func _on_draw_card_add_card():
 	var legal_cards = [7, 501, 505, 460, 499, 81, 114, 124, 215, 340, 344, 437, 480]
@@ -12,6 +13,7 @@ func _on_random_card_pressed():
 	var hand = get_node("Hand")
 	hand.add_random_card()
 
+# Creates Resources (.tres) for each card in the JSON
 func create_resources():
 	for card_id in 511:
 		$save_cards.initialize(card_id)
