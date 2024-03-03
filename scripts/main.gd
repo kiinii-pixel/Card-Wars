e49225075cfc8baf091628627848025bb97e5daa
kiinii <kiinii@proton.me>
2024-03-03 16:40:58 +0100

    Fighting!
    Barebones version of the fight button is working.

--- scripts/main.gd
+++ scripts/main.gd
@@ -26,15 +26,28 @@ func _on_reset_pressed():
 	$Deck.load_deck()
 
 func _on_fight_pressed():
-	var landscapes = $Landscapes
-	var enemy_landscapes = $EnemyLandscapes
-	print(56)
-	
-	var i = 0
-	for landscape in landscapes.get_children():
-		if landscape.get_child_count() == 4:
-			var new_def = landscape.get_child(3).def - enemy_landscapes.get_child(i).get_child(3).atk
-			landscape.get_child(3).%DefenseLabel.text = new_def
-		i += 1
+	var landscapes = $Landscapes # Node that holds 4 Landscapes
+	var enemy_landscapes = $EnemyLandscapes # Node that holds 4 (enemy) landscapes
+	var index = 0 # Used to count current Landscape in For Loop
 
-	
+	for landscape in landscapes.get_children(): # Goes through all Cards.
+		if landscape.get_child_count() == 4: # If there's a Card on the landscape
+			var creature = landscape.get_child(3) # Get the card / creature
+			var opposing_creature = enemy_landscapes.get_child(index).get_child(3)
+			if opposing_creature != null:
+				creature.def -= opposing_creature.atk
+				creature.get_node("%DefenseLabel").text = String.num_int64(creature.def)
+				opposing_creature.def -= creature.atk
+				opposing_creature.get_node("%DefenseLabel").text = String.num_int64(opposing_creature.def)
+				if creature.def <= 0:
+					creature.queue_free()
+				if opposing_creature.def <= 0:
+					opposing_creature.queue_free()
+			else:
+				pass
+				#Decrease Opponent's HP
+		else:
+			pass
+			#if enemy_landscape.get_child_count() == 4:
+			#decrease own health
+		index += 1
