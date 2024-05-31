d02184ecc781024fdc370bb9a8b89d1c6b4a91ce
kiinii <kiinii@proton.me>
2024-05-31 18:28:07 +0200

    Added Main Menu and Settings Menu

---
+++ scripts/menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func _on_decks_pressed():
+	pass # Replace with function body.
+
+func _on_settings_pressed():
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
---
+++ scripts/settings.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
