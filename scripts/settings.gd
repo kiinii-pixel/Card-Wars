3b06644a763c8116c2d91ed3be0518e71d44b56d
kiinii <kiinii@proton.me>
2024-06-01 21:07:58 +0200

    Worked on Settings Menu

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -1,4 +1,18 @@
 extends Control
 
+@onready var resolutions_button = $Options/VBoxContainer/OptionButton
+
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+var resolutions = {
+	"1920x1080" : Vector2i(1920, 1080),
+	"1366x768" : Vector2i(1366, 768)
+}
+
+func _ready():
+	add_resolutions()
+
+func add_resolutions():
+	for r in resolutions:
+		resolutions_button.add_item(r)
