de97d98e9eaca60b44159f38225a2dc36ddff275
kiinii <kiinii@proton.me>
2024-06-25 17:43:11 +0200

    Resolutions Menu working

--- scripts/ui/settings.gd
+++ scripts/ui/settings.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var resolutions_button = $Options/VBoxContainer/OptionButton
+@onready var resolutions_button = $Options/VBoxContainer/ResolutionOptionButton
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://scenes/ui/menu.tscn")
@@ -16,3 +16,14 @@ func _ready():
 func add_resolutions():
 	for r in resolutions:
 		resolutions_button.add_item(r)
+
+
+func _on_resolution_option_button_item_selected(index):
+	var key = resolutions_button.get_item_text(index)
+	get_window().set_size(resolutions[key])
+	center_window()
+
+func center_window():
+	var screen_center = DisplayServer.screen_get_position() + DisplayServer.screen_get_size() / 2
+	var window_size = get_window().get_size_with_decorations()
+	get_window().set_position(screen_center - window_size / 2)
