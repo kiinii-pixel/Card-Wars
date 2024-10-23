c4533409c161a63aeb3eaf5598d522c63caec212
kiinii <kiinii@proton.me>
2024-10-23 16:15:14 +0200

    Removed unneeded Code + Reimport Images

--- scripts/ui/deckbuilder.gd
+++ scripts/ui/deckbuilder.gd
@@ -4,23 +4,24 @@ extends Control
 const CARD : PackedScene = preload("res://scenes/objects/card.tscn")
 
 func _ready():
-	#load_deck()
-	add_card()
+	load_cards()
 
-func add_card():
+
+func load_cards():
 	var list = preloader.get_resource_list()
 	for instance in list:
-		var card = CARD.instantiate()
-		#card.custom_minimum_size = Vector2(693, 980)
-		card.data = preloader.get_resource(instance)
-		card.load_image()
-		if card.get_node("%CardImage").texture != null:
-			$ScrollContainer/GridContainer.add_child(card)
-			card.state_mashine.current_state = card.state_mashine.states["in_deck"]
-			card.drag_component.allow_drag = true
-			card.get_node("drag_component").scale_down(0.1)
-		else:
-			card.queue_free()
+		create_card(instance)
+
+
+func create_card(instance):
+	var card = CARD.instantiate()
+	card.data = preloader.get_resource(instance)
+	card.load_image()
+	if card.get_node("%CardImage").texture != null:
+		$ScrollContainer/GridContainer.add_child(card)
+		card.state_mashine.current_state = card.state_mashine.states["in_deck"]
+	else:
+		card.queue_free()
 
 
 func _on_back_pressed():
