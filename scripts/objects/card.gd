d00a0ffa0a0d5476cad297fd15cb55a1b0194306
kiinii <kiinii@proton.me>
2024-10-23 22:28:09 +0200

    Made Card Script more readable

--- scripts/objects/card.gd
+++ scripts/objects/card.gd
@@ -54,34 +54,40 @@ func load_values():
 	if data.card_type == "Creature":
 		var atk_label = %AttackLabel
 		var def_label = %DefenseLabel
-		if atk_label.text != String.num_int64(atk):
-			if String.num_int64(atk).length() > 1:
+
+		#region setting/adjusting the atk_label's text
+		if atk_label.text != String.num_int64(atk): # Set atk_label to atk
+			if String.num_int64(atk).length() > 1: # If atk has 2 digits
 				atk_label.add_theme_font_size_override("font_size", 58)
 			else:
 				atk_label.remove_theme_font_size_override("font_size")
 			atk_label.text = String.num_int64(atk)
-		if int(atk_label.text) < data.atk:
-			atk_label.add_theme_color_override("font_color", Color(1, 0, 0))
-		elif int(atk_label.text) > data.atk:
-			atk_label.add_theme_color_override("font_color", Color(0, 1, 0))
+			if int(atk_label.text) < data.atk: # If attack is lower than default
+				atk_label.add_theme_color_override("font_color", Color(1, 0, 0))
+			elif int(atk_label.text) > data.atk: # If attack is higher than default
+				atk_label.add_theme_color_override("font_color", Color(0, 1, 0))
+		#endregion
 
+		#region setting/adjusting the def_label's text
 		if def_label.text != String.num_int64(def):
 			if String.num_int64(def).length() > 1:
 				def_label.add_theme_font_size_override("font_size", 58)
 			else:
 				def_label.remove_theme_font_size_override("font_size")
 			def_label.text = String.num_int64(def)
-		if int(def_label.text) < data.def:
-			def_label.add_theme_color_override("font_color", Color(1, 0, 0))
-		elif int(def_label.text) > data.def:
-			def_label.add_theme_color_override("font_color", Color(0, 1, 0))
+			if int(def_label.text) < data.def:
+				def_label.add_theme_color_override("font_color", Color(1, 0, 0))
+			elif int(def_label.text) > data.def:
+				def_label.add_theme_color_override("font_color", Color(0, 1, 0))
+		#endregion
+
 	else:
 		%AttackLabel.text = ""
 		%DefenseLabel.text = ""
 
 # Reset Values to default
 func reset_values():
-	set_name(data.card_name) # sets name in the debug editor (instead of Node2D@1)
+	set_name(data.card_name) # Sets Debug Editor name (instead of Node2D@1)
 	# Atk/Def (when creature)
 	if data.card_type == "Creature":
 		atk = data.atk
@@ -99,8 +105,6 @@ func reset_values():
 func _on_drag_component_body_exited(body):
 	if body_ref == body:
 		is_inside = false
-		#if body ref = body exited
-		#landscape.get_child(3).queue_free()
 		if body.get_node_or_null("card_preview"):
 			body.get_node("card_preview").queue_free()
 
