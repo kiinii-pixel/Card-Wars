8d0a5d420bd1a2a585d80a2c11fffac6067816dc
kiinii <kiinii@proton.me>
2025-05-04 19:00:45 +0200

    Card Moving up Fix

--- scripts/states/overlapping.gd
+++ scripts/states/overlapping.gd
@@ -6,6 +6,7 @@ func enter():
 
 func update(_delta : float):
 	if Input.is_action_just_released("action_key"):
+		card.get_node("SubViewportContainer").position += Vector2(0, 50)
 		Transitioned.emit(self, "in_play")
 	elif !card.is_inside:
 		Transitioned.emit(self, "dragging")
