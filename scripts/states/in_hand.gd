bb646112b3820e0be1b4b51d2b187b879f20dce8
kiinii <kiinii@proton.me>
2025-05-05 11:11:20 +0200

    Added Shader for fake 3D

--- scripts/states/in_hand.gd
+++ scripts/states/in_hand.gd
@@ -3,10 +3,10 @@ class_name InHand extends State # Card is in Hand
 var clicked : bool
 
 func enter():
-	card.flip()
+	#card.flip_up()
 	card.drag_component.allow_drag = true
 	card.drag_component.selected = false
-	await drag_component.scale_down(0.2)
+	drag_component.scale_down(0.2)
 	card.z_index = 4
 	drag_component.mouse_entered.connect(_on_drag_component_mouse_entered)
 	drag_component.mouse_exited.connect(_on_drag_component_mouse_exited)
@@ -14,8 +14,9 @@ func enter():
 
 
 func update(_delta : float):
+	# When you click first and then hover over a card. It goes to hover after releasing the button.
 	if Input.is_action_just_released("action_key"):
-		if clicked:
+		if clicked and drag_component.selected:
 			drag_component.mouse_entered.disconnect(_on_drag_component_mouse_entered)
 			drag_component.mouse_exited.disconnect(_on_drag_component_mouse_exited)
 			Transitioned.emit(self, "hovering")
