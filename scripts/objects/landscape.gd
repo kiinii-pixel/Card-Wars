32f665216ff9f1b11d4e2793515a3ba8f254f16a
kiinii <kiinii@proton.me>
2026-03-09 20:07:53 +0100

    Added flip up and flip down functions to landscape along with face_down var

--- scripts/objects/landscape.gd
+++ scripts/objects/landscape.gd
@@ -1,6 +1,7 @@
 class_name Landscape extends Node2D
 
 var empty : bool = true
+var face_down : bool = false
 @export var art_variant: int = 3
 enum LANDSCAPES { Blue_Plains, Cornfields, IcyLands, NiceLands,
 SandyLands, Useless_Swamps }
@@ -16,9 +17,18 @@ func load_image():
 	var image_path: String = "res://assets/images/landscapes/" + str(landscape_type) + str(art_variant) + ".png"
 	%LandscapeImage.texture = load(image_path)
 
-
 func _on_child_order_changed() -> void:
 	if get_child_count() == 4:
 		empty = false
 	if get_child_count() == 3:
 		empty = true
+
+func flip_down() -> void:
+	%LandscapeImage.visible = false
+	%FaceDownImage.visible = true
+	face_down = true
+
+func flip_up() -> void:
+	%LandscapeImage.visible = true
+	%FaceDownImage.visible = false
+	face_down = false
